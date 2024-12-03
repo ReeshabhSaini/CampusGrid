e7e11efc8b07fb4c686b17dcf05b7913f2d6a990
Reeshabh Saini <reeshabhschd@gmail.com>
2024-12-03 17:56:34 +0530

    First Commit Setting up all the files

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./public/index.html"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#4CAF50",
+        secondary: "#8BC34A",
+      },
+    },
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/aspect-ratio'),
+  ],
+};
