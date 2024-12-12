69b8a5df0cb1931936ef62e05071f59f07932260
ZPEC1 <zavik0914@gmail.com>
2024-12-13 01:16:58 +0530

    LogIn page basic styling

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -22,33 +22,48 @@ const LoginPage = () => {
   };
 
   return (
-    <div className="login-page">
-      <h1>Login</h1>
-      <form onSubmit={handleLogin}>
-        <div>Email</div>
-        <input
-          type="email"
-          placeholder="username@pec.edu.in"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          required
-        />
-        <div>Password</div>
-        <input
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
-        />
-        <div>Forgot Password?</div>
-        <button type="submit">Sign in</button>
-        <div>or continue with</div>
-        <button>Sign in with Google</button>
-        <div>
-          Don't have an account yet? <span>Register</span>
-        </div>
-      </form>
+    <div className="login-container">
+      <div className="login-box">
+  <h1 class="login-subtitle">Login</h1>
+        <form onSubmit={handleLogin}>
+          <div className="form-group">
+            <label>Email</label>
+            <input
+              type="email"
+              placeholder="username@gmail.com"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+          </div>
+          <div className="form-group">
+            <label>Password</label>
+            <input
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+          </div>
+          <div className="forgot-password">
+            <a href="#">Forgot Password?</a>
+          </div>
+          <button type="submit" className="btn primary-btn">Sign in</button>
+          <div className="alternative">or continue with</div>
+          <button type="button" className="btn google-btn">
+  <img src= {`${process.env.PUBLIC_URL}/google-logo.png`} alt="Google Logo" />
+  Sign in with Google
+</button>
+
+          <div className="register">
+            Don't have an account yet? <a href="#">Register</a>
+          </div>
+        </form>
+      </div>
+      {/* <div className="login-logo">
+        <img src={`${process.env.PUBLIC_URL}/logo.jpg`} alt="Campus Grid Logo" />
+      </div> */}
     </div>
   );
 };
