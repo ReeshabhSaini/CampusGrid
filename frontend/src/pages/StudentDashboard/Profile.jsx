10508b9dea8309c8b0e67fab6f14751bf33d7fe9
Reeshabh Saini <reeshabhschd@gmail.com>
2025-01-12 15:22:04 +0530

    Student Edit Profile Changes

--- frontend/src/pages/StudentDashboard/Profile.jsx
+++ frontend/src/pages/StudentDashboard/Profile.jsx
@@ -66,224 +66,229 @@ const EditProfileModal = ({ studentData, onClose, onSubmit }) => {
   return (
     <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center">
       <div
-        className="p-6 rounded-lg w-1/2 shadow-md"
+        className="p-6 rounded-lg w-1/2 shadow-md flex flex-col"
         style={{ backgroundColor: "#f7ffcc" }}
       >
         <h3 className="text-2xl font-bold mb-4">Edit Profile</h3>
-        <form onSubmit={handleSubmit}>
-          <div>
-            <label
-              htmlFor="first_name"
-              className="block text-sm font-medium text-indigo-600"
-            >
-              First Name
-            </label>
-            <input
-              type="text"
-              id="first_name"
-              name="first_name"
-              value={formData.first_name}
-              onChange={handleChange}
-              className="w-full p-3 border rounded-md"
-            />
-          </div>
-          <div>
-            <label
-              htmlFor="last_name"
-              className="block text-sm font-medium text-indigo-600"
-            >
-              Last Name
-            </label>
-            <input
-              type="text"
-              id="last_name"
-              name="last_name"
-              value={formData.last_name}
-              onChange={handleChange}
-              className="w-full p-3 border rounded-md"
-            />
-          </div>
-          <div>
-            <label
-              htmlFor="email"
-              className="block text-sm font-medium text-indigo-600"
-            >
-              Email
-            </label>
-            <input
-              type="email"
-              id="email"
-              name="email"
-              value={formData.email}
-              onChange={handleChange}
-              className="w-full p-3 border rounded-md"
-            />
-          </div>
-          {/* Branch */}
-          <div className="mb-4">
-            <label className="block mb-2 text-sm font-medium text-indigo-600 text-left">
-              Branch
-            </label>
-            <select
-              name="branch"
-              onChange={handleChange}
-              value={studentData.branch}
-              className="w-full p-3 border rounded-md focus:ring-2 focus:ring-indigo-300 focus:outline-none"
-              required
-            >
-              <option value="" disabled>
-                Select Your Branch
-              </option>
-              <option value="CSE">Computer Science and Engineering</option>
-              <option value="CSE-DS">
-                Computer Science and Engineering (Data Science)
-              </option>
-              <option value="CSE-AI">
-                Computer Science and Engineering (Artificial Intelligence)
-              </option>
-              <option value="ECE">
-                Electronics and Communication Engineering
-              </option>
-              <option value="EE">Electrical Engineering</option>
-              <option value="Mech">Mechanical Engineering</option>
-              <option value="Civil">Civil Engineering</option>
-              <option value="Civil">Aerospace Engineering</option>
-              <option value="Meta">
-                Metallurgical and Materials Engineering
-              </option>
-              <option value="Prod">
-                Production and Industrial Engineering
-              </option>
-            </select>
-          </div>
-          {/* Semester */}
-          <div className="mb-4">
-            <label className="block mb-2 text-sm font-medium text-indigo-600 text-left">
-              Semester
-            </label>
-            <select
-              name="semester"
-              onChange={handleChange}
-              value={formData.semester}
-              className="w-full p-3 border rounded-md focus:ring-2 focus:ring-indigo-300 focus:outline-none"
-              required
-            >
-              <option value="" disabled>
-                Select Semester
-              </option>
-              <option value="1">1st</option>
-              <option value="2">2nd</option>
-              <option value="3">3rd</option>
-              <option value="4">4th</option>
-              <option value="5">5th</option>
-              <option value="6">6th</option>
-              <option value="7">7th</option>
-              <option value="8">8th</option>
-            </select>
-          </div>
-          {/* Class Group */}
-          <div className="mb-4">
-            <label className="block mb-2 text-sm font-medium text-indigo-600 text-left">
-              Class Group
-            </label>
-            <select
-              name="class_group"
-              onChange={handleChange}
-              value={studentData.class_group}
-              className="w-full p-3 border rounded-md focus:ring-2 focus:ring-indigo-300 focus:outline-none"
-              required
-            >
-              <option value="" disabled>
-                Select Class Group
-              </option>
-              <option value="G1">G1</option>
-              <option value="G2">G2</option>
-            </select>
-          </div>
+        <div className="overflow-y-auto max-h-96 px-2 mb-4">
+          {/* Form */}
+          <form onSubmit={handleSubmit} className="space-y-4">
+            <div>
+              <label
+                htmlFor="first_name"
+                className="block text-sm font-medium text-indigo-600"
+              >
+                First Name
+              </label>
+              <input
+                type="text"
+                id="first_name"
+                name="first_name"
+                value={formData.first_name}
+                onChange={handleChange}
+                className="w-full p-3 border rounded-md"
+              />
+            </div>
+            <div>
+              <label
+                htmlFor="last_name"
+                className="block text-sm font-medium text-indigo-600"
+              >
+                Last Name
+              </label>
+              <input
+                type="text"
+                id="last_name"
+                name="last_name"
+                value={formData.last_name}
+                onChange={handleChange}
+                className="w-full p-3 border rounded-md"
+              />
+            </div>
+            <div>
+              <label
+                htmlFor="email"
+                className="block text-sm font-medium text-indigo-600"
+              >
+                Email
+              </label>
+              <input
+                type="email"
+                id="email"
+                name="email"
+                value={formData.email}
+                onChange={handleChange}
+                className="w-full p-3 border rounded-md"
+              />
+            </div>
+            {/* Branch */}
+            <div>
+              <label className="block mb-2 text-sm font-medium text-indigo-600 text-left">
+                Branch
+              </label>
+              <select
+                name="branch"
+                onChange={handleChange}
+                value={formData.branch}
+                className="w-full p-3 border rounded-md focus:ring-2 focus:ring-indigo-300 focus:outline-none"
+                required
+              >
+                <option value="" disabled>
+                  Select Your Branch
+                </option>
+                <option value="CSE">Computer Science and Engineering</option>
+                <option value="CSE-DS">
+                  Computer Science and Engineering (Data Science)
+                </option>
+                <option value="CSE-AI">
+                  Computer Science and Engineering (Artificial Intelligence)
+                </option>
+                <option value="ECE">
+                  Electronics and Communication Engineering
+                </option>
+                <option value="EE">Electrical Engineering</option>
+                <option value="Mech">Mechanical Engineering</option>
+                <option value="Civil">Civil Engineering</option>
+                <option value="Civil">Aerospace Engineering</option>
+                <option value="Meta">
+                  Metallurgical and Materials Engineering
+                </option>
+                <option value="Prod">
+                  Production and Industrial Engineering
+                </option>
+              </select>
+            </div>
+            {/* Semester */}
+            <div className="mb-4">
+              <label className="block mb-2 text-sm font-medium text-indigo-600 text-left">
+                Semester
+              </label>
+              <select
+                name="semester"
+                onChange={handleChange}
+                value={formData.semester}
+                className="w-full p-3 border rounded-md focus:ring-2 focus:ring-indigo-300 focus:outline-none"
+                required
+              >
+                <option value="" disabled>
+                  Select Semester
+                </option>
+                <option value="1">1st</option>
+                <option value="2">2nd</option>
+                <option value="3">3rd</option>
+                <option value="4">4th</option>
+                <option value="5">5th</option>
+                <option value="6">6th</option>
+                <option value="7">7th</option>
+                <option value="8">8th</option>
+              </select>
+            </div>
+            {/* Class Group */}
+            <div className="mb-4">
+              <label className="block mb-2 text-sm font-medium text-indigo-600 text-left">
+                Class Group
+              </label>
+              <select
+                name="class_group"
+                onChange={handleChange}
+                value={studentData.class_group}
+                className="w-full p-3 border rounded-md focus:ring-2 focus:ring-indigo-300 focus:outline-none"
+                required
+              >
+                <option value="" disabled>
+                  Select Class Group
+                </option>
+                <option value="G1">G1</option>
+                <option value="G2">G2</option>
+              </select>
+            </div>
 
-          {/* Tutorial Group */}
-          <div className="mb-4">
-            <label className="block mb-2 text-sm font-medium text-indigo-600 text-left">
-              Tutorial Group
-            </label>
-            <select
-              name="tutorial_group"
-              onChange={handleChange}
-              value={studentData.tutorial_group}
-              className="w-full p-3 border rounded-md focus:ring-2 focus:ring-indigo-300 focus:outline-none"
-              required
-            >
-              <option value="" disabled>
-                Select Tutorial Group
-              </option>
-              <option value="G1">G1</option>
-              <option value="G2">G2</option>
-              <option value="G3">G3</option>
-              <option value="G4">G4</option>
-              <option value="G5">G5</option>
-              <option value="G6">G6</option>
-              <option value="G7">G7</option>
-              <option value="G8">G8</option>
-            </select>
-          </div>
+            {/* Tutorial Group */}
+            <div className="mb-4">
+              <label className="block mb-2 text-sm font-medium text-indigo-600 text-left">
+                Tutorial Group
+              </label>
+              <select
+                name="tutorial_group"
+                onChange={handleChange}
+                value={studentData.tutorial_group}
+                className="w-full p-3 border rounded-md focus:ring-2 focus:ring-indigo-300 focus:outline-none"
+                required
+              >
+                <option value="" disabled>
+                  Select Tutorial Group
+                </option>
+                <option value="G1">G1</option>
+                <option value="G2">G2</option>
+                <option value="G3">G3</option>
+                <option value="G4">G4</option>
+                <option value="G5">G5</option>
+                <option value="G6">G6</option>
+                <option value="G7">G7</option>
+                <option value="G8">G8</option>
+              </select>
+            </div>
 
-          {/* Lab Group */}
-          <div className="mb-4">
-            <label className="block mb-2 text-sm font-medium text-indigo-600 text-left">
-              Lab Group
-            </label>
-            <select
-              name="lab_group"
-              onChange={handleChange}
-              value={studentData.lab_group}
-              className="w-full p-3 border rounded-md focus:ring-2 focus:ring-indigo-300 focus:outline-none"
-              required
-            >
-              <option value="" disabled>
-                Select Lab Group
-              </option>
-              <option value="G1">G1</option>
-              <option value="G2">G2</option>
-              <option value="G3">G3</option>
-              <option value="G4">G4</option>
-              <option value="G5">G5</option>
-              <option value="G6">G6</option>
-              <option value="G7">G7</option>
-              <option value="G8">G8</option>
-            </select>
-          </div>
-          <div>
-            <label
-              htmlFor="student_id"
-              className="block text-sm font-medium text-indigo-600"
-            >
-              Student ID
-            </label>
-            <input
-              type="text"
-              id="student_id"
-              name="student_id"
-              value={formData.student_id}
-              onChange={handleChange}
-              className="w-full p-3 border rounded-md"
-            />
-          </div>
-          <div className="flex justify-between mt-4">
-            <button
-              type="button"
-              onClick={onClose}
-              className="bg-red-500 text-white px-6 py-2 rounded-lg hover:bg-red-600"
-            >
-              Cancel
-            </button>
-            <button
-              type="submit"
-              className="bg-blue-500 text-white px-6 py-2 rounded-lg hover:bg-blue-600"
-            >
-              Save Changes
-            </button>
-          </div>
-        </form>
+            {/* Lab Group */}
+            <div className="mb-4">
+              <label className="block mb-2 text-sm font-medium text-indigo-600 text-left">
+                Lab Group
+              </label>
+              <select
+                name="lab_group"
+                onChange={handleChange}
+                value={studentData.lab_group}
+                className="w-full p-3 border rounded-md focus:ring-2 focus:ring-indigo-300 focus:outline-none"
+                required
+              >
+                <option value="" disabled>
+                  Select Lab Group
+                </option>
+                <option value="G1">G1</option>
+                <option value="G2">G2</option>
+                <option value="G3">G3</option>
+                <option value="G4">G4</option>
+                <option value="G5">G5</option>
+                <option value="G6">G6</option>
+                <option value="G7">G7</option>
+                <option value="G8">G8</option>
+              </select>
+            </div>
+            <div>
+              <label
+                htmlFor="student_id"
+                className="block text-sm font-medium text-indigo-600"
+              >
+                Student ID
+              </label>
+              <input
+                type="text"
+                id="student_id"
+                name="student_id"
+                value={formData.student_id}
+                onChange={handleChange}
+                className="w-full p-3 border rounded-md"
+              />
+            </div>
+          </form>
+        </div>
+        {/* Buttons */}
+        <div className="flex justify-between mt-4 border-t pt-4">
+          <button
+            type="button"
+            onClick={onClose}
+            className="bg-red-500 text-white px-6 py-2 rounded-lg hover:bg-red-600"
+          >
+            Cancel
+          </button>
+          <button
+            type="submit"
+            className="bg-blue-500 text-white px-6 py-2 rounded-lg hover:bg-blue-600"
+            onClick={handleSubmit}
+          >
+            Save Changes
+          </button>
+        </div>
       </div>
     </div>
   );
